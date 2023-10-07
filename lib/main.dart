96d6988e71a67de51e79943c0b756dc0787bfc5d
Eusuf Uddin <eusuf15-8926@diu.edu.bd>
2023-10-07 15:38:44 +0600

    Button with add style

--- lib/main.dart
+++ lib/main.dart
@@ -14,36 +14,56 @@ class MyApp extends StatelessWidget {
       title: 'Button Demo',
       theme: ThemeData(primarySwatch: Colors.deepOrange),
       debugShowCheckedModeBanner: false,
-      home:  MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-   MyHomePage({super.key});
+  MyHomePage({super.key});
 
   final ButtonStyle buttonStyle = ElevatedButton.styleFrom(
-    foregroundColor: Colors.white,
-    backgroundColor: Colors.green
-  );
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.green,
+      elevation: 6,
+      shadowColor: Colors.black,
+      padding: EdgeInsets.all(12),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12))));
+
+  SnakbarMsg(String msg, context) {
+    return ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(msg)));
+  }
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     appBar: AppBar(
-       title: Text("Button Demo"),
-     ),
-     body: Row(
-       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-       children: [
-         TextButton(onPressed: (){}, child: Text("Text Button")),
-         ElevatedButton(onPressed: (){}, child: Text("Text Button")),
-         OutlinedButton(onPressed: (){}, child: Text("Text Button")),
-
-       ],
-     ),
-   );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Button Demo"),
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          TextButton(
+              onPressed: () {
+                SnakbarMsg("TextButton cliked", context);
+              },
+              child: Text("Text Button")),
+          ElevatedButton(
+            onPressed: () {
+              SnakbarMsg("ElevatedButton cliked", context);
+            },
+            child: Text("ElevatedButton"),
+            style: buttonStyle,
+          ),
+          OutlinedButton(
+              onPressed: () {
+                SnakbarMsg("OutlinedButton cliked", context);
+              },
+              child: Text("OutlinedButton")),
+        ],
+      ),
+    );
   }
-
-
 }
